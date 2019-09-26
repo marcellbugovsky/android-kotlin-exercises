3a4f2ea57d93fded1ec11b0450712683b77e4d25
Marcell Bugovsky <m.bugovsky@gmail.com>
2019-09-26 16:32:03 +0200

    "Track my workday quality" initial commit

---
+++ TrackMyWorkdayQuality/app/src/main/java/com/android/example/trackmyworkdayquality/screens/quality/QualityFragment.kt
@@ -0,0 +1,26 @@
+package com.android.example.trackmyworkdayquality.screens.quality
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.android.example.trackmyworkdayquality.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class QualityFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.quality_fragment, container, false)
+    }
+
+
+}
