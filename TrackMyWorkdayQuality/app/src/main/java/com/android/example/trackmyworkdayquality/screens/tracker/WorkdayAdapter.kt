1c3afb8104b5b0d45c042231f47c1f39ac60602c
Marcell Bugovsky <m.bugovsky@gmail.com>
2019-10-07 17:05:56 +0200

    refactor: refactored viewHolder functions

--- TrackMyWorkdayQuality/app/src/main/java/com/android/example/trackmyworkdayquality/screens/tracker/WorkdayAdapter.kt
+++ TrackMyWorkdayQuality/app/src/main/java/com/android/example/trackmyworkdayquality/screens/tracker/WorkdayAdapter.kt
@@ -1,6 +1,5 @@
 package com.android.example.trackmyworkdayquality.screens.tracker
 
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,7 +7,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.android.example.trackmyworkdayquality.R
-import com.android.example.trackmyworkdayquality.TextItemViewHolder
 import com.android.example.trackmyworkdayquality.convertDurationToFormatted
 import com.android.example.trackmyworkdayquality.convertNumericQualityToString
 import com.android.example.trackmyworkdayquality.database.Workday
@@ -23,31 +21,54 @@ class WorkdayAdapter: RecyclerView.Adapter<WorkdayAdapter.ViewHolder>() {
 
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int
+    ) {
         val item = data[position]
-        val res = holder.itemView.context.resources
-        holder.workdayLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-        holder.quality.text = convertNumericQualityToString(item.workdayQuality, res)
-        holder.qualityImage.setImageResource(when (item.workdayQuality) {
-            0 -> R.drawable.ic_quality_0
-            1 -> R.drawable.ic_quality_1
-            2 -> R.drawable.ic_quality_2
-            3 -> R.drawable.ic_quality_3
-            4 -> R.drawable.ic_quality_4
-            else -> R.drawable.ic_workday_active
-        })
+        holder.bind(item)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.list_item_workday, parent, false)
-        return ViewHolder(view)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        return ViewHolder.from(parent)
     }
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    class ViewHolder private constructor(
+        itemView: View
+    ): RecyclerView.ViewHolder(itemView) {
         val workdayLength: TextView = itemView.findViewById(R.id.workday_length)
         val quality: TextView = itemView.findViewById(R.id.quality_string)
         val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+
+        fun bind(
+            item: Workday
+        ) {
+            val res = itemView.context.resources
+            workdayLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+            quality.text = convertNumericQualityToString(item.workdayQuality, res)
+            qualityImage.setImageResource(
+                when (item.workdayQuality) {
+                    0 -> R.drawable.ic_quality_0
+                    1 -> R.drawable.ic_quality_1
+                    2 -> R.drawable.ic_quality_2
+                    3 -> R.drawable.ic_quality_3
+                    4 -> R.drawable.ic_quality_4
+                    else -> R.drawable.ic_workday_active
+                }
+            )
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.list_item_workday, parent, false)
+                return ViewHolder(view)
+            }
+        }
+
     }
 
 }
