7b57d7d2c1116b08a631fec9d3da0953aae6feb0
Marcell Bugovsky <m.bugovsky@gmail.com>
2019-10-07 07:59:14 +0200

    Implemented the new list_item layout

--- TrackMyWorkdayQuality/app/src/main/java/com/android/example/trackmyworkdayquality/screens/tracker/WorkdayAdapter.kt
+++ TrackMyWorkdayQuality/app/src/main/java/com/android/example/trackmyworkdayquality/screens/tracker/WorkdayAdapter.kt
@@ -2,14 +2,18 @@ package com.android.example.trackmyworkdayquality.screens.tracker
 
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.android.example.trackmyworkdayquality.R
 import com.android.example.trackmyworkdayquality.TextItemViewHolder
+import com.android.example.trackmyworkdayquality.convertDurationToFormatted
+import com.android.example.trackmyworkdayquality.convertNumericQualityToString
 import com.android.example.trackmyworkdayquality.database.Workday
 
-class WorkdayAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+class WorkdayAdapter: RecyclerView.Adapter<WorkdayAdapter.ViewHolder>() {
 
     var data = listOf<Workday>()
         set(value) {
@@ -19,22 +23,31 @@ class WorkdayAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
 
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.workdayQuality.toString()
-
-        if (item.workdayQuality <= 1) {
-            holder.textView.setTextColor(Color.RED)
-        } else {
-            holder.textView.setTextColor(Color.WHITE)
-        }
-
+        val res = holder.itemView.context.resources
+        holder.workdayLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+        holder.quality.text = convertNumericQualityToString(item.workdayQuality, res)
+        holder.qualityImage.setImageResource(when (item.workdayQuality) {
+            0 -> R.drawable.ic_quality_0
+            1 -> R.drawable.ic_quality_1
+            2 -> R.drawable.ic_quality_2
+            3 -> R.drawable.ic_quality_3
+            4 -> R.drawable.ic_quality_4
+            else -> R.drawable.ic_workday_active
+        })
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
-        return TextItemViewHolder(view)
+        val view = layoutInflater.inflate(R.layout.list_item_workday, parent, false)
+        return ViewHolder(view)
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val workdayLength: TextView = itemView.findViewById(R.id.workday_length)
+        val quality: TextView = itemView.findViewById(R.id.quality_string)
+        val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
     }
 
 }
