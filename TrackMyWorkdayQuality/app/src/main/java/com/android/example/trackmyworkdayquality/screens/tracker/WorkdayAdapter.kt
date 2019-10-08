90c66b6ac5dc7717f44426236eaa2a143b3a6a54
Marcell Bugovsky <m.bugovsky@gmail.com>
2019-10-08 07:07:29 +0200

    chore: added databinding to list item

--- TrackMyWorkdayQuality/app/src/main/java/com/android/example/trackmyworkdayquality/screens/tracker/WorkdayAdapter.kt
+++ TrackMyWorkdayQuality/app/src/main/java/com/android/example/trackmyworkdayquality/screens/tracker/WorkdayAdapter.kt
@@ -1,10 +1,7 @@
 package com.android.example.trackmyworkdayquality.screens.tracker
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +9,7 @@ import com.android.example.trackmyworkdayquality.R
 import com.android.example.trackmyworkdayquality.convertDurationToFormatted
 import com.android.example.trackmyworkdayquality.convertNumericQualityToString
 import com.android.example.trackmyworkdayquality.database.Workday
+import com.android.example.trackmyworkdayquality.databinding.ListItemWorkdayBinding
 
 class WorkdayAdapter : ListAdapter<Workday, WorkdayAdapter.ViewHolder>(WorkdayDiffCallback()) {
 
@@ -43,19 +41,16 @@ class WorkdayAdapter : ListAdapter<Workday, WorkdayAdapter.ViewHolder>(WorkdayDi
 
 
     class ViewHolder private constructor(
-        itemView: View
-    ): RecyclerView.ViewHolder(itemView) {
-        val workdayLength: TextView = itemView.findViewById(R.id.workday_length)
-        val quality: TextView = itemView.findViewById(R.id.quality_string)
-        val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+        val binding: ListItemWorkdayBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(
             item: Workday
         ) {
             val res = itemView.context.resources
-            workdayLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-            quality.text = convertNumericQualityToString(item.workdayQuality, res)
-            qualityImage.setImageResource(
+            binding.workdayLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+            binding.qualityString.text = convertNumericQualityToString(item.workdayQuality, res)
+            binding.qualityImage.setImageResource(
                 when (item.workdayQuality) {
                     0 -> R.drawable.ic_quality_0
                     1 -> R.drawable.ic_quality_1
@@ -70,8 +65,8 @@ class WorkdayAdapter : ListAdapter<Workday, WorkdayAdapter.ViewHolder>(WorkdayDi
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.list_item_workday, parent, false)
-                return ViewHolder(view)
+                val binding = ListItemWorkdayBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
             }
         }
 
